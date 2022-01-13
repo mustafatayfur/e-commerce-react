b9a59c67d6d5862bb9aef5b150cbbd5e5a052154
mustafa <mltm0204@gmail.com>
2022-01-13 09:30:13 +0300

    ....

---
+++ src/components/CartItem/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style"
+// import { formatPrice } from '../utils/helpers'
+// import AmountButtons from './AmountButtons'
+// import { FaTrash } from 'react-icons/fa'
+// import { useCartContext } from '../context/cart_context'
+const CartItem = () => {
+  return <h4>cart item</h4>
+}
+
+
+
+export default CartItem
