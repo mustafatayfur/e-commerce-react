caed5f26296a99f93653be647ce7816f4cfc5be4
mustafa <mltm0204@gmail.com>
2022-01-13 09:35:25 +0300

    ...

---
+++ src/components/CartTotal/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./style"
+// import { useCartContext } from '../context/cart_context'
+// import { useUserContext } from '../context/user_context'
+// import { formatPrice } from '../utils/helpers'
+// import { Link } from 'react-router-dom'
+
+const CartTotals = () => {
+  return <h4>cart totals</h4>
+}
+
+
+
+export default CartTotals
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./style"
+const Contact = () => {
+  return <h4>contact section</h4>
+}
+
+
+export default Contact
