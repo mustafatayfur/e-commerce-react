f0332d449693553f2912019e3c0ac149f10875a7
mustafa <mltm0204@gmail.com>
2022-01-13 09:25:42 +0300

    ...

---
+++ src/components/CartButton/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./style"
+// import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
+// import { Link } from 'react-router-dom'
+// import { useProductsContext } from '../context/products_context'
+// import { useCartContext } from '../context/cart_context'
+// import { useUserContext } from '../context/user_context'
+
+const CartButtons = () => {
+  return <h4>cart buttons </h4>
+}
+
+
+export default CartButtons
