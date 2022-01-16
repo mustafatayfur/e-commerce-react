4207d45b8ecfcd01f0beecd64ecf990d4f5019cf
mustafa <mltm0204@gmail.com>
2022-01-16 22:00:54 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import styled from 'styled-components'
 
+
+
 const Button = styled.button`
 background: green;
 color: white;
@@ -16,18 +18,10 @@ color: white;
 
 function App() {
   return (
-    <div className="App">
-      <h4>comfy sloth starter</h4>
-      <Button>Click me</Button>
-      <Container>
-        <div>
-            <h3>hello world</h3>
-        </div>
-        <div className="hero">
-          hero text
-        </div>
-      </Container>
-    </div>
+    <Router>
+      
+        <Home/>
+    </Router>
   );
 }
 
